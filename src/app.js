ee62ed3f9bbd3b42405d818524a93f844e1d0761
Patrick Gustafson <patrickgus@charter.net>
2020-01-10 12:39:52 -0800

    import arcticles-service

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const ArticlesService = require("./articles-service");
 
 const app = express();
 
@@ -12,6 +13,14 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.get("/articles", (req, res, next) => {
+  ArticlesService.getAllArticles(/* need knex instance here */)
+    .then(articles => {
+      res.json(articles);
+    })
+    .catch(next);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
