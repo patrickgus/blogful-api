e49b81980a27f9b84edecefcf8143279b08a3d6b
Patrick Gustafson <patrickgus@charter.net>
2020-01-16 10:02:32 +0000

    add author to articles

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -11,7 +11,8 @@ const serializeArticle = article => ({
   style: article.style,
   title: xss(article.title),
   content: xss(article.content),
-  date_published: article.date_published
+  date_published: article.date_published,
+  author: article.author
 });
 
 articlesRouter
@@ -25,7 +26,7 @@ articlesRouter
       .catch(next);
   })
   .post(jsonParser, (req, res, next) => {
-    const { title, content, style } = req.body;
+    const { title, content, style, author } = req.body;
     const newArticle = { title, content, style };
 
     for (const [key, value] of Object.entries(newArticle))
@@ -33,7 +34,7 @@ articlesRouter
         return res.status(400).json({
           error: { message: `Missing '${key}' in request body` }
         });
-
+    newArticle.author = author;
     ArticlesService.insertArticle(req.app.get("db"), newArticle)
       .then(article => {
         res
